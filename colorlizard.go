d2133460ef012dd2022255b347762706ccc8d425
amitdix <amit.dixititbhu@gmail.com>
2019-03-11 18:23:16 +0530

    fix build

---
+++ colorlizard.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/color-lizard/config"
+
+	"github.com/color-lizard/internal/controller"
+
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ready := true
+	// Read the ENV variables
+	var endpointMap map[string]config.Endpoint
+	err := config.ReadMockEndpointsData(&endpointMap)
+	if err != nil {
+		log.Fatal().Msg("Unable To Read Mappings")
+	}
+	router := controller.GetRouter(endpointMap, &ready)
+
+	http.ListenAndServe(":8881", router)
+	log.Error().Err(err).Msg("Exited")
+
+}
